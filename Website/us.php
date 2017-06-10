b2c8c43732fc57a6674db88f0e35f40f99aaa86c
Vedad Fejzagic <vfejzagic1@etf.unsa.ba>
2017-06-10 17:38:22 +0200

    Dodan bootstrap. Dodana tabela. Uljepsan kod.

--- Website/us.php
+++ Website/us.php
@@ -33,13 +33,78 @@
 	    return $result;
 	} 
 
-	$conn = ConnectDb();
-	$value = GetDataFromURI();
-	if(InsertDataToTableDB($value, $conn)){
-		echo "<h1>DATA SENT: ".$value." </h1>";
+	function ShowTable($conn){
+
+		$result = SelectDataFromTableDB($conn);
+
+		echo '
+		<div class="col-lg-4 col-md-4 col-xs-12 mx-auto">
+			<br>  
+			   	<table class = "table table-bordered table-hover table-sm">
+			 	 <thead class="thead-default"> 
+				   <tr>
+				    <th>Vrijeme</th>
+					<th>Temperatura</th>
+				   </tr>
+			     </thead>';
+		
+		while($row = $result->fetch_assoc()){
+			echo '<tr>';
+			echo '<td>'.$row['Vrijeme'].'</td>';
+			echo '<td>'.$row['Temperatura']. '</td>';
+			echo '</tr>';
+		}
+		echo '</table></div>';
+
 	}
 
-	$conn->close();
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" >
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >       
+
+    <title>cijena.ba</title>
+
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+		
+
+  </head>
+  <body>
+
+	<?php
+
+		$conn = ConnectDb();
+		ShowTable($conn);
+
+		$value = GetDataFromURI();
+		if(InsertDataToTableDB($value, $conn)){
+			echo "<h1>DATA SENT: ".$value." </h1>";
+		}
+
+		$conn->close();
+
+
+	?>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
